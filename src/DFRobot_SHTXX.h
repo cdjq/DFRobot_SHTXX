6b560174b9c99e283d916e09ce5b6eef30176eb8
fary99 <71751335+fary99@users.noreply.github.com>
2021-04-07 08:22:49 +0800

    Update DFRobot_SHTXX.h

--- src/DFRobot_SHTXX.h
+++ src/DFRobot_SHTXX.h
@@ -93,12 +93,12 @@ public:
    * @n                    PRECISION_HIGH_HEATER_OFF                                 measure T & RH with high precision (high repeatability) 
    * @n                    PRECISION_MID_HEATER_OFF                                  measure T & RH with medium precision (medium repeatability)
    * @n                    PRECISION_LOW_HEATER_OFF                                  measure T & RH with lowest precision (low repeatability) 
-   * @n                    PRECISION_HIGH_HEATER_1S                                  activate highest heater power & high precis. meas. (typ. 200mW @ 3.3V) for 1s 
-   * @n                    PRECISION_HIGH_HEATER_100MS                               activate highest heater power & high precis. meas. (typ. 200mW @ 3.3V) for 0.1s
-   * @n                    PRECISION_MID_HEATER_1S                                   activate medium heater power  & high precis. meas. (typ. 110mW @ 3.3V) for 1s 
-   * @n                    PRECISION_MID_HEATER_100MS                                activate medium heater power  & high precis. meas. (typ. 110mW @ 3.3V) for 0.1s 
-   * @n                    PRECISION_LOW_HEATER_1S                                   activate lowest heater power  & high precis. meas. (typ. 20mW @ 3.3V) for 1s 
-   * @n                    PRECISION_LOW_HEATER_100MS                                activate lowest heater power  & high precis. meas. (typ. 20mW @ 3.3V) for 0.1s 
+   * @n                    PRECISION_HIGH_HEATER_1S                                  activate highest heater power & high precis. meas. for 1s 
+   * @n                    PRECISION_HIGH_HEATER_100MS                               activate highest heater power & high precis. meas. for 0.1s
+   * @n                    PRECISION_MID_HEATER_1S                                   activate medium heater power  & high precis. meas. for 1s 
+   * @n                    PRECISION_MID_HEATER_100MS                                activate medium heater power  & high precis. meas. for 0.1s 
+   * @n                    PRECISION_LOW_HEATER_1S                                   activate lowest heater power  & high precis. meas. for 1s 
+   * @n                    PRECISION_LOW_HEATER_100MS                                activate lowest heater power  & high precis. meas. for 0.1s 
    */
    void setMode(uint16_t mode) ;
 
